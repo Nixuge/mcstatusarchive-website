81b3951d49d1cb7824cd43cffb88eec85a899a07
Nixuge <mail@nixuge.me>
2023-11-19 20:17:54 +0100

    Graph displays properly to time now

--- src/components/ServerViewer/PlayercountGraph.vue
+++ src/components/ServerViewer/PlayercountGraph.vue
@@ -6,6 +6,7 @@ import { onMounted, onUnmounted, ref, watch, type Ref } from 'vue';
 // Needed for chart to render
 import { Chart as chartjs, registerables } from 'chart.js';
 chartjs.register(...registerables);
+import 'chartjs-adapter-luxon'
 
 import { useSnapshots } from '@/stores/serverviewer/snapshots';
 const { getServerSnapshotsForDateRange } = useSnapshots();
@@ -28,7 +29,8 @@ const options: any = {
     },
     scales: {
         x: {
-            display: false // Hide X axis labels
+            display: false, // Hide X axis labels
+            type: 'time'
         }
     }   
 }
@@ -48,15 +50,34 @@ function updateGraph() {
         oneEvery = Math.floor((snapshotsLen / playerStatsDiv.value.clientWidth));
     
     let lastPlayersOn = Number.NaN;
-    let snapshot = null;
+    let lastSaveTime = Number.MAX_VALUE;
     let labels = []
     let playerCount = []
     let playerAverageList = []
     for (let i = 0; i < snapshots.length; i++) {
-        snapshot = snapshots[i];
-        lastPlayersOn = (snapshot.players_on) ? snapshot.players_on : lastPlayersOn;
-        playerAverageList.push(lastPlayersOn);
+        const snapshot = snapshots[i];
+        
+        // Fill in empty parts
+        const diff = (snapshot.save_time - lastSaveTime)
+        if (diff > 60*60) { // 1h+ of downtime
+            const insts = Math.floor(diff / 60)
+            for (let i2 = 0; i2 < insts; i2++) {
+                if (i2 % oneEvery != 0)
+                    continue
+                const newTime = (lastSaveTime + (60 * (i2+1))) * 1000;
+                
+                labels.push(newTime);
+                playerCount.push(NaN);
+            }
+        }
+        
 
+        lastSaveTime = snapshot.save_time;
+        
+        if (snapshot.players_on)
+            lastPlayersOn = snapshot.players_on;
+        
+        playerAverageList.push(lastPlayersOn);
         if (i % oneEvery != 0)
             continue
 
@@ -68,14 +89,13 @@ function updateGraph() {
         const playerAverage = sum / playerAverageList.length;
         playerAverageList = [];
 
-        labels.push(snapshot.save_date.toLocaleString("fr-FR"))
+        labels.push(snapshot.save_time * 1000)
         playerCount.push(playerAverage)
     }
 
-    data.value ={
+    data.value = {
         labels: labels,
-        datasets: [
-          {
+        datasets: [{
             label: 'Players on',
             borderColor: '#fc9802',
             backgroundColor: '#fcad02',
@@ -85,10 +105,11 @@ function updateGraph() {
             pointHitRadius: 5,
             pointRadius: 0,
             pointBorderWidth: 0,
-            pointHoverRadius: 4
-          }
-        ]
-      }
+            borderWidth: 1,
+            pointHoverRadius: 4,
+            spanGaps: false // only works if you fill in your data with NaNs
+        }]
+    }
 }
 
 watch(getServerSnapshotsForDateRange, () => {
