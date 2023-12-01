a344fa66693c31f83dba8bbe246bb1bd0cdffa2d
Nixuge <mail@nixuge.me>
2023-12-02 00:17:37 +0100

    Better trycatch for parseMotd

--- src/ts/utils/motd.ts
+++ src/ts/utils/motd.ts
@@ -1,23 +1,26 @@
 import { textToHTML, JSONToHTML } from '@sfirew/minecraft-motd-parser'
 
 
+// Multiple reasons this can happen:
+// - The MOTD is in Python dict format (an issue I did at first) and not in standard JSON,
+// hence why parsing it throws an error
+// - The MOTD is simply a string that starts with { (unlikely but possible)
+// --- this check is here for performances reason tho, I don't want to try and parse everything as json
+// --- before falling back if it fails.
+function doCatch(func: Function, toParse: string) {
+    try {
+        return func(toParse);
+    } catch(error) {
+        return textToHTML("§cFailed to parse MOTD: " + error);
+    }
+}
+
 export function parseMotd(motd: string | undefined, nullMessage?: string) {
     if (motd === null || motd === undefined) {
-        return textToHTML((nullMessage) ? nullMessage : "§4Can't connect to server");
+        return doCatch(textToHTML, (nullMessage) ? nullMessage : "§4Can't connect to server")
     } else if (motd[0] == '{') {
-        try {
-            const data = JSONToHTML(JSON.parse(motd));            
-            return data;
-        } catch (error) {
-            // Multiple reasons this can happen:
-            // - The MOTD is in Python dict format (an issue I did at first) and not in standard JSON,
-            // hence why parsing it throws an error
-            // - The MOTD is simply a string that starts with { (unlikely but possible)
-            // --- this check is here for performances reason tho, I don't want to try and parse everything as json
-            // --- before falling back if it fails.
-            return textToHTML("§cFailed to parse MOTD: " + error);
-        }
+        return doCatch(JSONToHTML, JSON.parse(motd))
     } else {
-        return textToHTML(motd);
+        return doCatch(textToHTML, motd);
     }
 }
